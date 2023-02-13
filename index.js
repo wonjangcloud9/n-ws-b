396ac2ce259ef791905a054cb06c166df8bc3a91
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-02-13 10:49:49 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//👇🏻 New imports
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "<http://localhost:4000>",
+  },
+});
+
+//👇🏻 Add this before the app.get() block
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+  socket.on("disconnect", () => {
+    socket.disconnect();
+    console.log("🔥: A user disconnected");
+  });
+});
